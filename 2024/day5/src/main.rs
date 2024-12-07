355bd934a7e5f740edf59569ed906a68192fcc91
adsick <adsick@protonmail.com>
2024-12-07 22:26:13 +0200

    day 5 part 2

--- 2024/day5/src/main.rs
+++ 2024/day5/src/main.rs
@@ -23,13 +23,14 @@ fn main() {
     }
 
     let mut sum: u32 = 0;
+    let mut incorrect = vec![];
 
     for line in lines {
         let mut met = vec![];
 
-        'l: for n in line.split(',') {
-            let n: u8 = n.parse().unwrap();
+        let numbers: Vec<u8> = line.split(',').map(|n| n.parse().unwrap()).collect();
 
+        'l: for n in &numbers {
             if let Some(forbidden) = rules.get(&n) {
                 for m in &met {
                     if forbidden.contains(m) {
@@ -39,11 +40,48 @@ fn main() {
                 }
             }
 
-            met.push(n);
+            met.push(*n);
         }
 
-        met.get(met.len() / 2).map(|v| sum += *v as u32);
+        met.get(met.len() / 2)
+            .map(|v| sum += *v as u32)
+            .or_else(|| Some(incorrect.push(numbers)));
     }
 
-    println!("ans: {sum}");
+    println!("ans1: {sum}\n");
+
+    // bubble sort kind of a solution
+    for l in &mut incorrect {
+        let mut flag = true;
+        while flag {
+            flag = false;
+            'i: for i in 1..l.len() {
+                for j in 0..i {
+                    let a = l[i];
+                    let b = l[j];
+
+                    if let Some(forbidden) = rules.get(&a) {
+                        if forbidden.contains(&b) {
+                            print!("{l:?} -> ");
+                            l.swap(i, j);
+                            println!("{l:?} (swapped {} with {})", l[j], l[i]);
+                            flag = true;
+                            break 'i;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let ans2: u32 = incorrect
+        .iter()
+        .map(|l| *l.get(l.len() / 2).unwrap() as u32)
+        .sum();
+    println!("ans2: {ans2}")
 }
+
+// 0  1  2  3  4     0  1  2  3  4
+// 75,47,61,53,29 -> 97,75,47,61,53
+// 61,13,29       -> 61,29,13
+// 97,13,75,29,47 -> 97,75,47,29,13
