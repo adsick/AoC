f0bfb216e048df76c4918558d33485dc636488a8
adsick <adsick@protonmail.com>
2024-12-13 10:13:27 +0200

    day13 part 1

--- 2024/day13/src/main.rs
+++ 2024/day13/src/main.rs
@@ -1,9 +1,9 @@
-use std::ops::Add;
 use std::fmt::Debug;
+use std::ops::{Add, Mul};
 
-const INPUT: &str = include_str!("../small.txt");
+const INPUT: &str = include_str!("../input.txt");
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Default)]
 struct Pos(u32, u32);
 
 impl From<(u32, u32)> for Pos {
@@ -20,6 +20,14 @@ impl Add for Pos {
     }
 }
 
+impl Mul<u32> for Pos {
+    type Output = Pos;
+
+    fn mul(self, rhs: u32) -> Self::Output {
+        Pos(self.0 * rhs, self.1 * rhs)
+    }
+}
+
 impl PartialEq for Pos {
     fn eq(&self, other: &Self) -> bool {
         self.0 == other.0 && self.1 == other.1
@@ -32,47 +40,66 @@ impl Debug for Pos {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Task {
     a: Pos,
     b: Pos,
     d: Pos,
 }
 
-fn parse(input: &str) -> Task {
+fn parse(input: &str) -> Option<Task> {
     let mut lines = input.lines();
     let mut ps = [Pos(0, 0); 3];
     for i in 0..3 {
-
         let op = if i < 2 { "+" } else { "=" };
         let del = (format!("X{op}"), format!("Y{op}"));
 
         let p: Pos = lines
-        .next()
-        .unwrap()
-        .split_once(&del.0)
-        .unwrap()
-        .1
-        .split_once(", ")
-        .map(|(x, y)| {
-            (
-                x.parse().unwrap(),
-                y.split_once(&del.1).unwrap().1.parse().unwrap(),
-            )
-        })
-        .unwrap()
-        .into();
+            .next()?
+            .split_once(&del.0)
+            .unwrap()
+            .1
+            .split_once(", ")
+            .map(|(x, y)| {
+                (
+                    x.parse().unwrap(),
+                    y.split_once(&del.1).unwrap().1.parse().unwrap(),
+                )
+            })
+            .unwrap()
+            .into();
         ps[i] = p;
     }
     let a: Pos = ps[0];
     let b: Pos = ps[1];
     let d: Pos = ps[2];
 
-    Task { a, b, d }
+    Some(Task { a, b, d })
+}
+
+fn solve1(t: Task) -> Option<u32> {
+    let mut c = None;
+
+    for b in (0..100).rev() {
+        for a in (0..100).rev() {
+            if t.a * a + t.b * b == t.d {
+                c = Some(3 * a + b)
+            }
+        }
+    }
+
+    c
 }
 
 fn main() {
+    let mut ans1 = 0;
     for t in INPUT.split("\n\n") {
-        println!("{:?}", parse(t));
+        let Some(t) = parse(t) else {
+            break
+        };
+        let c = solve1(t);
+        ans1 += c.unwrap_or_default();
+        // println!("{:?} = {:?}", t, c);
     }
+    println!("ans1: {ans1}");
 }
